f76021c07a185b651f70da38bbaa88fab0b5a1e1
Motahar <rimon.raton@gmail.com>
2021-04-05 14:31:26 +0600

    Route split

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -59,8 +59,11 @@ class RouteServiceProvider extends ServiceProvider
     protected function mapWebRoutes()
     {
         Route::middleware('web')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/web.php'));
+            ->namespace($this->namespace)
+            ->group(function ($router){
+                require base_path( 'routes/web.php');
+                require base_path( 'routes/admin.php');
+            });
     }
 
     /**
