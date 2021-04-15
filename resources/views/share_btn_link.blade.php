7fcc3aa2d37dfc4c10720178bd24cbc5928c0930
Motaharul <rimon.raton@gmail.com>
2021-04-15 12:54:06 +0600

    share link

--- resources/views/share_btn_link.blade.php
+++ resources/views/share_btn_link.blade.php
@@ -1,6 +1,11 @@
 <!doctype html>
 <html>
 <head>
+    <meta property="og:title" content="Gyankosh Challenge" />
+    <meta property="og:url" content="https://gyankosh.org" />
+    <meta property="og:image" content="https://gyankosh.org/img/quiz/as.jpg" />
+    <meta property="og:description" content="জ্ঞানের সমৃদ্ধি জ্ঞানকোষের সাথে" />
+    <meta property="og:site_name" content="Gyankosh" />
     <script src="https://platform-api.sharethis.com/js/sharethis.js#property=5e896e693790270019b8aac5&product=inline-share-buttons"></script>
 </head>
 <body>
