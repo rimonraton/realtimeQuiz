d2c2efcf21c6315de33e9f8ae48561c1a067cf8c
Motahar <rimon.raton@gmail.com>
2021-06-10 16:06:35 +0600

    Whatsapp share test with open graph tag

--- resources/views/share_btn_link_challenge.blade.php
+++ resources/views/share_btn_link_challenge.blade.php
@@ -1,6 +1,13 @@
 <!doctype html>
 <html>
 <head>
+    <meta property="og:title" content="Gyankosh Quiz" />
+    <meta property="og:site_name" content="Gyankosh" />
+    <meta property="og:url" content="{{url('challengeShareResult/'.$game .'/'.$id.'/'.$uid)}}/share" />
+    <meta property="og:image" content="https://gyankosh.org/images/logobe.png" />
+    <meta property="og:description"   content="Gyankosh জ্ঞানের সমৃদ্ধি জ্ঞানকোষের সাথে " />
+    <meta property="og:type='article'"  content="website জ্ঞানের সমৃদ্ধি জ্ঞানকোষের সাথে type article. " />
+
     <script src="https://platform-api.sharethis.com/js/sharethis.js#property=5e896e693790270019b8aac5&product=inline-share-buttons"></script>
 </head>
 <body>
--- routes/games.php
+++ routes/games.php
@@ -1,4 +1,5 @@
 <?php
+use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Game\ChallengeController;
 
