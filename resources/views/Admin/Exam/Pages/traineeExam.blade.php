f6b1cb8059bbb1ca15587c70c1b4cd04869270d8
zulfiker ali <zulfiker.phy@gmail.com>
2023-02-16 11:51:36 +0600

    some message changes

--- resources/views/Admin/Exam/Pages/traineeExam.blade.php
+++ resources/views/Admin/Exam/Pages/traineeExam.blade.php
@@ -197,7 +197,47 @@
                                 @if($futureDate > $now)
                                     @if(auth()->user()->roleuser->role->id > 3)
                                         <p>{{$lang == 'gb' ? 'Time left for the exam to start' : 'পরীক্ষা শুরু হতে সময় বাকী'}}: {{$lang == 'gb' ? $remainingTime : $ban->bn_human($remainingTime) }}</p>
+                                    <p>
+                                        <span class="btn btn-sm btn-outline-primary rounded-lg align-self-center text-primary cursor-not-allowed">
+                                             {{__('msg.not_published_yet')}}
+                                        </span>
+                                    </p>
                                     @endif
+                                @else
+                                @if(auth()->user()->roleuser->role->id > 3)
+                                    @if($futureDate < $now)
+                                        @if($exam->results_count)
+                                            {{--                                @if(auth()->user()->roleuser->role->id < 4)--}}
+                                            {{--                                    <a href="{{ url('exam-result/'. $exam->id) }}" class="btn btn-xs btn-outline-danger align-self-center">--}}
+                                            {{--                                        {{ $lang == 'gb' ? 'View the results' : 'ফলাফল দেখুন ' }}--}}
+                                            {{--                                    </a>--}}
+                                            {{--                                    @endif--}}
+                                            <p>
+                                    <span class="btn btn-sm btn-outline-success rounded-lg align-self-center text-success cursor-not-allowed">
+                                        {{__('exam.submitted')}}
+                                    </span>
+                                            </p>
+                                        @else
+                                            @if($exam->is_published)
+                                                <a href="{{ url('start-exams/'. $exam->id . '/' . Auth::id()) }}" class="btn btn-sm btn-outline-info align-self-center rounded-lg" >
+                                                    {{__('msg.start')}}
+                                                </a>
+                                            @else
+                                                @if($futureDate < $now)
+                                                    <p>
+                                                <span class="btn btn-sm btn-outline-danger rounded-lg align-self-center text-danger cursor-not-allowed">
+                                                    {{__('exam.expired')}}
+                                                </span>
+                                                    </p>
+                                                @else
+                                                    <span class="btn btn-xs btn-outline-primary align-self-center text-primary cursor-not-allowed">
+                                                        {{__('msg.not_published_yet')}}
+                                                    </span>
+                                                @endif
+                                            @endif
+                                        @endif
+                                    @endif
+                                @endif
                                 @endif
                                 @if($exam->results_count)
                                         @if(auth()->user()->roleuser->role->id < 4)
@@ -211,42 +251,42 @@
                                 @endif
                         </div>
 {{--                    </a>--}}
-                    <div class="info d-flex justify-content-center py-1 px-2 mt-auto ">
-                        @if(auth()->user()->roleuser->role->id > 3)
-                        @if($futureDate < $now)
-                        @if($exam->results_count)
+{{--                    <div class="info d-flex justify-content-center py-1 px-2 mt-auto ">--}}
+{{--                        @if(auth()->user()->roleuser->role->id > 3)--}}
+{{--                        @if($futureDate < $now)--}}
+{{--                        @if($exam->results_count)--}}
 {{--                                @if(auth()->user()->roleuser->role->id < 4)--}}
 {{--                                    <a href="{{ url('exam-result/'. $exam->id) }}" class="btn btn-xs btn-outline-danger align-self-center">--}}
 {{--                                        {{ $lang == 'gb' ? 'View the results' : 'ফলাফল দেখুন ' }}--}}
 {{--                                    </a>--}}
 {{--                                    @endif--}}
-                                <p>
-                                    <span class="btn btn-sm btn-outline-danger rounded-lg align-self-center text-danger cursor-not-allowed">
-                                        {{__('exam.submitted')}}
-                                    </span>
-                                </p>
-                            @else
-                                    @if($exam->is_published)
-                                    <a href="{{ url('start-exams/'. $exam->id . '/' . Auth::id()) }}" class="btn btn-xs btn-outline-info align-self-center" >
-                                        {{__('msg.start')}}
-                                    </a>
-                                    @else
-                                        @if($futureDate < $now)
-                                            <p>
-                                                <span class="btn btn-sm btn-outline-danger rounded-lg align-self-center text-danger cursor-not-allowed">
-                                                    {{__('exam.expired')}}
-                                                </span>
-                                            </p>
-                                        @else
-                                            <a class="btn btn-xs btn-outline-dark align-self-center text-warning cursor-not-allowed">
-                                                {{__('msg.not_published_yet')}}
-                                            </a>
-                                        @endif
-                                    @endif
-                                @endif
-                            @endif
-                        @endif
-                    </div>
+{{--                                <p>--}}
+{{--                                    <span class="btn btn-sm btn-outline-success rounded-lg align-self-center text-success cursor-not-allowed">--}}
+{{--                                        {{__('exam.submitted')}}--}}
+{{--                                    </span>--}}
+{{--                                </p>--}}
+{{--                            @else--}}
+{{--                                    @if($exam->is_published)--}}
+{{--                                    <a href="{{ url('start-exams/'. $exam->id . '/' . Auth::id()) }}" class="btn btn-xs btn-outline-info align-self-center" >--}}
+{{--                                        {{__('msg.start')}}--}}
+{{--                                    </a>--}}
+{{--                                    @else--}}
+{{--                                        @if($futureDate < $now)--}}
+{{--                                            <p>--}}
+{{--                                                <span class="btn btn-sm btn-outline-danger rounded-lg align-self-center text-danger cursor-not-allowed">--}}
+{{--                                                    {{__('exam.expired')}}--}}
+{{--                                                </span>--}}
+{{--                                            </p>--}}
+{{--                                        @else--}}
+{{--                                            <a class="btn btn-xs btn-outline-dark align-self-center text-warning cursor-not-allowed">--}}
+{{--                                                {{__('msg.not_published_yet')}}--}}
+{{--                                            </a>--}}
+{{--                                        @endif--}}
+{{--                                    @endif--}}
+{{--                                @endif--}}
+{{--                            @endif--}}
+{{--                        @endif--}}
+{{--                    </div>--}}
                 </div>
             </div>
         @endforeach
