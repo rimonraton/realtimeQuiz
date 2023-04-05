460279edb8a86db27c5d319a5a0a6e01ac1a0c45
rimonraton <rimon.raton@gmail.com>
2023-04-05 14:49:16 +0600

    Single Mode fix audio video problem an QR test

--- resources/views/LandingPage/landing_page.blade.php
+++ resources/views/LandingPage/landing_page.blade.php
@@ -613,18 +613,18 @@
             $('#camScan').toggle();
             const html5QrCode = new Html5Qrcode("reader");
             const qrCodeSuccessCallback = (decodedText, decodedResult) => {
-                console.log(decodedText, decodedResult)
+                alert(decodedText, decodedResult)
             };
             const config = { fps: 10, qrbox: { width: 250, height: 250 } };
 
-            // If you want to prefer front camera
-            html5QrCode.start({ facingMode: "user" }, config, qrCodeSuccessCallback);
-
-            // If you want to prefer back camera
-            html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
-
-            // Select front camera or fail with `OverconstrainedError`.
-            html5QrCode.start({ facingMode: { exact: "user"} }, config, qrCodeSuccessCallback);
+            // // If you want to prefer front camera
+            // html5QrCode.start({ facingMode: "user" }, config, qrCodeSuccessCallback);
+            //
+            // // If you want to prefer back camera
+            // html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
+            //
+            // // Select front camera or fail with `OverconstrainedError`.
+            // html5QrCode.start({ facingMode: { exact: "user"} }, config, qrCodeSuccessCallback);
 
             // Select back camera or fail with `OverconstrainedError`.
             html5QrCode.start({ facingMode: { exact: "environment"} }, config, qrCodeSuccessCallback);
