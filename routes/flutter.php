ce080a0bf18e072d946726b0e879d22f68ecc928
rimonraton <rimon.raton@gmail.com>
2024-08-11 15:56:22 +0600

    Flutter route and Controller separated.

--- routes/flutter.php
+++ routes/flutter.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\FlutterController;
 use Illuminate\Support\Facades\Route;
 
 
-//Route::get('getLoginFromFlutter/{random}/{user}/{email}', [FlutterController::class, 'getLoginFromFlutter']);
 Route::post('loginFlutter', [FlutterController::class, 'loginFlutter']);
 Route::post('resetPassword', [FlutterController::class, 'resetPassword']);
 Route::post('registration', [FlutterController::class, 'registerFlutter']);
